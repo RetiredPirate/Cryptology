70d684de2ff7831a699b7f2426b157d4b8d3f063
Andrew Williams <stuffmanx@gmail.com>
2017-09-24 16:28:49 -0700

    count each letter in cipher text

--- crypto.py
+++ crypto.py
@@ -8,10 +8,27 @@ def maEncrypt(plainText, cipher = lambda x: (x**5)%26, aMap = 1):
     if type(plainText) is not str:
         return "Invalid Input, Must be a String"
     plainText = plainText.lower().replace(" ","")
+
     cipherText = []
     for letter in plainText:
         cipherText.append(alphabet[cipher(alphabet.index(letter)+aMap)%26-aMap])
     print(''.join(cipherText).upper())
 
 def maDecrypt(cipherText):
-    
+    if type(cipherText) is not str:
+        return "Invalid Input, Must be a String"
+    cipherText = cipherText.lower().replace(" ","")
+
+    letterCount = []
+    for letter in alphabet:
+        letterCount.append(cipherText.count(letter))
+    print letterCount
+
+    maxLetterCount = max(letterCount)
+    x = [i for i,j in enumerate(letterCount) if j == maxLetterCount]
+    print(alphabet[x[0]])
+    print(x)
+
+
+
+maDecrypt("abcdeed")
