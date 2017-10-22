5c61f7f8c6737a66d4e39dd009f155e727fb3d6c
Andrew Williams <stuffmanx@gmail.com>
2017-10-22 16:31:27 -0700

    Fixing the decrypt function

--- crypto.py
+++ crypto.py
@@ -3,12 +3,7 @@ import sys
 from string import ascii_lowercase as alphabet
 import string
 import re
-<<<<<<< HEAD
-import numpy
-import pylatex
-=======
 # import numpy
->>>>>>> 5f93bc6ff62325b001aca7b6d5c293f411ae4c10
 
 
 def maEncrypt(plainText, cipher = lambda x: (x**5)%26, aMap = 1):
@@ -20,25 +15,23 @@ def maEncrypt(plainText, cipher = lambda x: (x**5)%26, aMap = 1):
         cipherText.append(alphabet[cipher(letter)%26-aMap])
     print(''.join(cipherText).upper())
 
-<<<<<<< HEAD
 def maDecrypt(cipherText, aMap = 1):
     cipherText = stringCheck(cipherText)
-=======
-def maDecrypt(cipherText, aMap = 1): # currently assumes aMap = 0
-    if type(cipherText) is not str:
-        return "Invalid Input, Must be a String"
-    cipherText = cipherText.lower().replace(" ","")
->>>>>>> 5f93bc6ff62325b001aca7b6d5c293f411ae4c10
 
     letterCount = []
     for letter in alphabet:
         letterCount.append(cipherText.count(letter))
-    print(letterCount)
+    # print(letterCount)
 
-    maxLetterCount = max(letterCount)
-    x = [i for i,j in enumerate(letterCount) if j == maxLetterCount]
-    print(alphabet[x[0]])
-    print(x)
+    # maxLetterCount = max(letterCount)
+    # x = [i for i,j in enumerate(letterCount) if j == maxLetterCount]
+    printList = []
+    for letter in range(len(alphabet)):
+        printList.append((alphabet[letter],letterCount[letter]))
+
+    # sorted(printList, key = lambda x: x[1])
+    print(reversed(sorted(printList, key = lambda x: x[1])))
+    # print(alphabet[x[0]])
 
 def numMap(text, aMap = 1):
     text = stringCheck(text)
@@ -70,29 +63,25 @@ def hillDecrypt(cipherText, aMap = 1):
 
 
 
-print(maEncrypt("lzf rrf yrp hol dno ilo mnf mkn rld njd nji nyo axp ybo fgc hpb \
-        yhf rno ipy bzf rmk non kxu ffk maf fup hof vyg nan hop rlo noi npy bny \
-        vpo xfu zfr mkn onu ffk h", lambda x: (9*x+9)%26 ))
-print((18*21)%26)
+# print(maEncrypt("lzf rrf yrp hol dno ilo mnf mkn rld njd nji nyo axp ybo fgc hpb \
+#         yhf rno ipy bzf rmk non kxu ffk maf fup hof vyg nan hop rlo noi npy bny \
+#         vpo xfu zfr mkn onu ffk h", lambda x: (x-11)%26 ))
+# print((18*21)%26)
+#
 
-
-<<<<<<< HEAD
 print(spaceString(numMap("ESP PYR TYP PCE STY VDZ QST DPB FLE TZY DLD LYL AAC ZIT \
         XLE TZY EZC PLW TEJ ESP ASJ DTN TDE EST YVD CPL WTE JTD LYL AAC ZIT XLE TZY \
          EZS TDP BFL ETZ YDE SPX LES PXL ETN TLY OZP DYE NLC P", aMap = 0)))
 
-maDecrypt("ESP PYR TYP PCE STY VDZ QST DPB FLE TZY DLD LYL AAC ZIT \
-        XLE TZY EZC PLW TEJ ESP ASJ DTN TDE EST YVD CPL WTE JTD LYL AAC ZIT XLE TZY \
-         EZS TDP BFL ETZ YDE SPX LES PXL ETN TLY OZP DYE NLC P")
+maDecrypt("MFE RLH WSR LHW BZN BNW SRX DEC INQ RNW JHL RBW BNL DER HQN DEQ \
+                BUJ WSH UZS RNN LDE RDA HJH LQC RWS HUM DTR EHU ICH NWN CDU JRE \
+                WSH ULD UDM DCP BWN AER RBW ZHU QRM CHP RIH UPX SRE RHE ZSB LRI \
+                RNI BIB WBU HQH WSW FQ")
 
 
 maEncrypt("ESP PYR TYP PCE STY VDZ QST DPB FLE TZY DLD LYL AAC ZIT \
         XLE TZY EZC PLW TEJ ESP ASJ DTN TDE EST YVD CPL WTE JTD LYL AAC ZIT XLE TZY \
-         EZS TDP BFL ETZ YDE SPX LES PXL ETN TLY OZP DYE NLC P", lambda x: (25*x-23)%26)
+         EZS TDP BFL ETZ YDE SPX LES PXL ETN TLY OZP DYE NLC P", lambda x: (x-11)%26 )
 
 # print((-18)%26)
 # print(maEncrypt("HGH FJL IOF YJJ TXN YJI HON VHC DJL IOW XFY XXT XON V", lambda x: (3*(x - 13))%26 , aMap = 0))
-=======
-# print(numMap("abcdefghijklmnopqrstuvwxyz", aMap = 0))
-# print(numMap("abcdefghijklmnopqrstuvwxyz", aMap = 1))
->>>>>>> 2481adecd52191ddf465b30a11ea27025ec1c842
