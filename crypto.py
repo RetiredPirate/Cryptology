4f3073fbf822e341fc88f8e657ed4d9444aee283
AWilliams18 <williaan18@up.edu>
2017-10-25 09:19:38 -0700

    Pre-merge commit

--- crypto.py
+++ crypto.py
@@ -3,12 +3,9 @@ import sys
 from string import ascii_lowercase as alphabet
 import string
 import re
-<<<<<<< HEAD
-import numpy
-import pylatex
-=======
+# import pylatex
 # import numpy
->>>>>>> 5f93bc6ff62325b001aca7b6d5c293f411ae4c10
+
 
 
 def maEncrypt(plainText, cipher = lambda x: (x**5)%26, aMap = 1):
@@ -18,27 +15,39 @@ def maEncrypt(plainText, cipher = lambda x: (x**5)%26, aMap = 1):
     cipherText = []
     for letter in plainText:
         cipherText.append(alphabet[cipher(letter)%26-aMap])
-    print(''.join(cipherText).upper())
+    return (''.join(cipherText).upper())
 
-<<<<<<< HEAD
-def maDecrypt(cipherText, aMap = 1):
+def freqInfo(cipherText, aMap = 1):
     cipherText = stringCheck(cipherText)
-=======
-def maDecrypt(cipherText, aMap = 1): # currently assumes aMap = 0
-    if type(cipherText) is not str:
-        return "Invalid Input, Must be a String"
-    cipherText = cipherText.lower().replace(" ","")
->>>>>>> 5f93bc6ff62325b001aca7b6d5c293f411ae4c10
 
     letterCount = []
     for letter in alphabet:
         letterCount.append(cipherText.count(letter))
-    print(letterCount)
 
     maxLetterCount = max(letterCount)
     x = [i for i,j in enumerate(letterCount) if j == maxLetterCount]
     print(alphabet[x[0]])
     print(x)
+
+def digraphFreq(cipherText, aMap = 1):
+    cipherText = numMap(stringCheck(cipherText))
+
+
+
+
+# def kasiskiTest(cipherText, aMap=1):
+#     cipherText = numMap(stringCheck(cipherText))
+#
+#     listOfSubstrings = []
+#     for substringLen in range(3,len(cipherText)):
+#         for num in range(0, len(cipherText)-substringLen):
+#             newString = cipherText[num:num+substringLen]
+#             if newString not in listOfSubstrings:
+#                 listOfSubstrings.append((newString, 0))
+#             else:
+#                 listOfSubstrings[index(newString)]
+
+    return listOfSubstrings
 
 def numMap(text, aMap = 1):
     text = stringCheck(text)
@@ -61,20 +70,54 @@ def spaceString(list):
     for number in list:
         newString += " " + str(number)
 
+    return newString
+
 def hillDecrypt(cipherText, aMap = 1):
     text = stringCheck(text)
     text = numMap(text, aMap)
 
 
+def vignereEncrypt(plainText, keyWord, aMap = 1):
+    plainText = numMap(stringCheck(plainText), aMap)
+    keyWord = numMap(stringCheck(keyWord), aMap)
+
+    keyLen = len(keyWord)
+    cipherText = ""
+
+    for i in range(0,len(plainText)):
+        cipherText += alphabet[plainText[i] + keyWord[i%keyLen]]
+
+    return cipherText
+
+def vignereDecrypt(plainText, keyWord, aMap = 1):
+    plainText = numMap(stringCheck(plainText), aMap)
+    keyWord = numMap(stringCheck(keyWord), aMap)
+
+    keyLen = len(keyWord)
+    cipherText = ""
+
+    for i in range(0,len(plainText)):
+        cipherText += alphabet[plainText[i] - keyWord[i%keyLen]]
+
+    return cipherText
 
 
 
 
-print(maEncrypt("lzf rrf yrp hol dno ilo mnf mkn rld njd nji nyo axp ybo fgc hpb \
-        yhf rno ipy bzf rmk non kxu ffk maf fup hof vyg nan hop rlo noi npy bny \
-        vpo xfu zfr mkn onu ffk h", lambda x: (9*x+9)%26 ))
-print((18*21)%26)
+#
+# print(vignereEncrypt("cheesyfeet", "apple", aMap = 0))
+#
+# print(vignereDecrypt("TUL VCA CTY CFW PGC BW", "berry", aMap = 0))
 
+# print(kasiskiTest("lzf rrf yrp hol dno ilo mnf mkn rld njd nji nyo axp ybo fgc hpb \
+#         yhf rno ipy bzf rmk non kxu ffk maf fup hof vyg nan hop rlo noi npy bny \
+#         vpo xfu zfr mkn onu ffk h"))
+# print((19*4)%26)
+
+# print(spaceString(numMap("KFH YYG IGM CEJ SST EBO EUG RWJ TSD VYK ZOZ LIZ KFH XKU UIC WXF WJG AXQ PBQ AGV GXD VDG UEV GMI GYK QQP IPS CLL FYP MUL KFH XPM HGM EVD KAV YQC EGU EAL YYY ZSZ MPX ZOC TXT RIM DID VDG SXO ZFF TSM EDV MEI MDV MPK OUJ KOD UBO AXB OOR SLP ZCW IMD VYG JWM IFQ", aMap = 1)))
+# freqInfo("KFH YYG IGM CEJ SST EBO EUG RWJ TSD VYK ZOZ LIZ KFH XKU UIC WXF WJG AXQ PBQ AGV GXD VDG UEV GMI GYK QQP IPS CLL FYP MUL KFH XPM HGM EVD KAV YQC EGU EAL YYY ZSZ MPX ZOC TXT RIM DID VDG SXO ZFF TSM EDV MEI MDV MPK OUJ KOD UBO AXB OOR SLP ZCW IMD VYG JWM IFQ")
+
+print(maEncrypt("a common mistake", lambda x: ((x * 7) + 5)%26).lower())
 
 # print(numMap("abcdefghijklmnopqrstuvwxyz", aMap = 0))
 # print(numMap("abcdefghijklmnopqrstuvwxyz", aMap = 1))
